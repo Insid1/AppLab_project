c56d2c5c320846641a7714298f3c85d29fbe6d26
Oleg <insid1@mail.ru>
2021-10-25 20:02:58 +1200

    small js code refactoring (not rly good one)

--- js/script.js
+++ js/script.js
@@ -1,28 +1,17 @@
 'use strict'
 
+const objNodes = {
+    burgerButton: document.querySelector('.burger-button'),
+    burgerMenu: document.querySelector('.header__navbar'),
 
-// -------- START code to animated tariff switch and button --------
+    headerButtons: document.body.querySelectorAll('a[data-goto]'),
 
-// Nodes кнопки
-const toggleButton = document.querySelector('.toggle-button');
-const toggleLighter = toggleButton.querySelector('.toggle-button__lighter')
-const toggleButtonSpans = toggleButton.querySelectorAll('span');
-// Nodes тарифов
-const monthPlane = document.querySelector('div.price-list.month');
-const yearPlane = document.querySelector('.price-list.year');
+    answersList: document.querySelectorAll('.asked-questions__answer'),
+    questionList: document.querySelectorAll('.asked-questions__question'),
+    answerButton: document.querySelectorAll('.asked-questions__button'),
 
-// Смена css классов по toggle
-function switchClassinToggle() {
-    toggleLighter.classList.toggle('moved');
-    toggleButtonSpans[0].classList.toggle('selected');
-    toggleButtonSpans[1].classList.toggle('selected');
-    monthPlane.classList.toggle('active');
-    yearPlane.classList.toggle('active');
 }
 
-toggleButton.addEventListener('click', switchClassinToggle)
-
-// -------- START code to animated tariff switch and button --------
 
 
 // -------- START code to make interactive banner --------
@@ -32,15 +21,19 @@ const movingBanner = document.querySelector('.banner');
 const innerContainer = movingBanner.querySelector('.banner__inner-container');
 const innerElements = innerContainer.querySelectorAll('li');
 
+// buttons
+const buttonScrollLeft = movingBanner.querySelector('.banner-scrolling-buttons__left');
+const buttonScrollRight = movingBanner.querySelector('.banner-scrolling-buttons__right');
+
 // Width of content inside banner
 let totalInnerWidth = 0;
+
 innerElements.forEach(value => {
     totalInnerWidth += value.offsetWidth;
 })
 
 // Coord to move in the banner
 let translateXCoord = 0;
-
 let increment = 270;
 
 function bannerScrollRight() {
@@ -57,74 +50,78 @@ function bannerScrollLeft() {
     innerContainer.style.transform = `translateX(${translateXCoord}px)`;
 }
 
-// buttons
-const buttonScrollLeft = movingBanner.querySelector('.banner-scrolling-buttons__left');
-const buttonScrollRight = movingBanner.querySelector('.banner-scrolling-buttons__right');
-
 buttonScrollLeft.addEventListener('click', bannerScrollLeft);
 buttonScrollRight.addEventListener('click', bannerScrollRight);
 
 // -------- END code to make interactive banner --------
 
-// -------- START code to make Animated floating list --------
 
-const answersList = document.querySelectorAll('.asked-questions__answer');
-const questionList = document.querySelectorAll('.asked-questions__question');
-const answerButton = document.querySelectorAll('.asked-questions__button');
 
-for (let i = 0; i < answerButton.length; i++) {
+// -------- START code to animate tariff switching and button --------
 
-    questionList[i].addEventListener('click', function () {
-        answersList[i].classList.toggle('_active');
-        answerButton[i].classList.toggle('_active');
-    })
+// Nodes кнопки
+const toggleButton = document.querySelector('.toggle-button');
+const toggleLighter = toggleButton.querySelector('.toggle-button__lighter')
+const toggleButtonSpans = toggleButton.querySelectorAll('span');
 
+// Nodes тарифов
+const monthPlane = document.querySelector('div.price-list.month');
+const yearPlane = document.querySelector('.price-list.year');
+
+// Смена css классов по toggle
+function switchClassinToggle() {
+    toggleLighter.classList.toggle('moved');
+    toggleButtonSpans[0].classList.toggle('selected');
+    toggleButtonSpans[1].classList.toggle('selected');
+    monthPlane.classList.toggle('active');
+    yearPlane.classList.toggle('active');
 }
 
-// -------- END code to make Animated floating list --------
-
-
-// -------- START code to scroll menu Buttons --------
-
-// сначало получить кнопки для скролинга
-// потом получить  координаты блоков с классом указанным в кнопках скроллинга в атрибусе data-goto
-
-const headerButtons = document.body.querySelectorAll('a[data-goto]')
-
-
-
-for (let aButton of headerButtons) {
-
-    aButton.addEventListener('click', function () {
-        let currClassName = aButton.dataset.goto;
-        const currScrollToDiv = document.body.querySelector('.' + currClassName);
-        let currYCord = currScrollToDiv.offsetTop;
-        scrollTo({
-            left: 0,
-            top: (currYCord - document.querySelector('header').offsetHeight),
-            behavior: "smooth",
-        })
-
-        // to close burger menu if clicked
-        burgerButton.classList.remove('active');
-        burgerMenu.classList.remove('active');
-        document.body.classList.remove('active');
-    })
-}
-
-
-// -------- END code to scroll menu Buttons --------
-
-// -------- START burgerMenu --------
-const burgerButton = document.querySelector('.burger-button');
-const burgerMenu = document.querySelector('.header__navbar');
-
-burgerButton.addEventListener('click', showBurgerMenu)
+// -------- END code to animate tariff switching and button --------
 
 function showBurgerMenu() {
-    burgerButton.classList.toggle('active');
-    burgerMenu.classList.toggle('active');
+    objNodes.burgerButton.classList.toggle('active');
+    objNodes.burgerMenu.classList.toggle('active');
     document.body.classList.toggle('active');
 }
-// -------- END burgerMenu --------
+
+function makeHeaderButtonsScrollable() {
+    for (let aButton of objNodes.headerButtons) {
+
+        aButton.addEventListener('click', function () {
+            let currClassName = aButton.dataset.goto;
+            const currScrollToDiv = document.body.querySelector('.' + currClassName);
+            let currYCord = currScrollToDiv.offsetTop;
+            scrollTo({
+                left: 0,
+                top: (currYCord - document.querySelector('header').offsetHeight),
+                behavior: "smooth",
+            })
+
+            // to close burger menu if clicked
+            objNodes.burgerButton.classList.remove('active');
+            objNodes.burgerMenu.classList.remove('active');
+            document.body.classList.remove('active');
+        })
+    }
+}
+
+function accordeon() {
+    for (let i = 0; i < objNodes.answerButton.length; i++) {
+
+        objNodes.questionList[i].addEventListener('click', function () {
+            objNodes.answersList[i].classList.toggle('_active');
+            objNodes.answerButton[i].classList.toggle('_active');
+        })
+    }
+}
+
+
+// Scripts to run to activate
+
+objNodes.burgerButton.addEventListener('click', showBurgerMenu) // burger button on slick start
+toggleButton.addEventListener('click', switchClassinToggle)
+
+makeHeaderButtonsScrollable() // start script of scrolaable buttons
+accordeon() // start accordeon for faq questions
 
--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 # Small pet project written with native JS only.
 
+Javacript is used to create general dynamic of the webPage and interactive user Interface.
+
 Design borrowed from  https://dribbble.com/shots/8641810-Freebie-App-landing-page.
 
-Education purpose only. Nofdg comertial use.
+Education purpose only. No for comertial use.
